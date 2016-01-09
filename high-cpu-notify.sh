09e3a6d7f60a52b836b25a34dea7a6f8de0d1663
Haroen Viaene <hello@haroen.me>
2016-01-09 10:44:01 +0100

    add script

---
+++ high-cpu-notify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# needs brew install terminal-notifier
+# source http://apple.stackexchange.com/a/90295/98431
+
+cpulimit=50
+prefix=${TMPDIR}cron_cpu
+current=$(ps -erco %cpu,command | tail -n+2 | sed 's/^ *//')
+echo "$current" > $prefix$(date +%s)
+a=($prefix*); for ((i=0;i<=${#a[@]}-3;i++)); do rm "${a[i]}"; done
+[[ $(awk '{s+=$1}END{printf "%i",s}' <<< "$current") -lt $cpulimit ]] && exit
+averages=$(awk '{cpu=$1;sub(/[^ ]+ /,"");a[$0]+=cpu;c[$0]++}END{for(i in a){printf "%.1f %s\n",a[i]/c[$0],i}}' $prefix* | sort -rn)
+if [[ $(awk '{s+=$1}END{printf "%i",s}' <<< "$averages") -ge $cpulimit ]]; then
+    terminal-notifier -title "CPU use" -message "$(head -n5 <<< "$averages" | paste -sd / -)"
+fi
